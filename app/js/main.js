cbaf1c76d50552e32c014ee650769b95b921703b
Serega414 <maksishkosergej@gmail.com>
2021-01-09 15:51:32 +0300

    added page.html,notification__box,message__box,basket__box,user-box> added menu_list drop down in hover, header__menu in  added menu__list-drop in hover, adaptive header and footer page.html,index.html,new-items.html

--- app/js/main.js
+++ app/js/main.js
@@ -43,5 +43,12 @@ $(function () {
 		$('.price-range__list').slideToggle();
 	});
 
+	$('.menu__btn').on('click', function(){
+		$('.menu__list').slideToggle();
+	});
+	$('.header__btn-menu').on('click', function(){
+		$('.header-box').toggleClass('active');
+	});
+
 	var mixer = mixitup('.products__inner');
 });
